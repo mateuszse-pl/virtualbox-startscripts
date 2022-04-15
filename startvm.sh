b112b8b2027470375dca50e753098e253fd86642
Mateusz Serafiński <70181520+mateuszse-pl@users.noreply.github.com>
2022-04-16 00:42:42 +0200

    Add files via upload

--- startvm.sh
+++ startvm.sh
@@ -1,5 +1,5 @@
 @echo off
-vboxmanage startvm virtualbox-podstawy
+vboxmanage startvm virtualbox-podstawy --type headless
 echo --------------------------------
 echo uruchomiono maszyne wirtualna 
 timeout 5
